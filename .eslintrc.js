3acbbaf59aac218e011297cd21d68e203238f1a2
myroslav-kolomiiets <mir.kolomiets@gmail.com>
2026-03-29 14:18:01 +0300

    Fix linter deps build error

--- .eslintrc.js
+++ .eslintrc.js
@@ -8,7 +8,6 @@ module.exports = {
     'plugin:react/recommended',
     'plugin:react-hooks/recommended',
     'plugin:jsx-a11y/recommended',
-    'plugin:import/recommended',
     'plugin:import/typescript',
     'plugin:prettier/recommended',
   ],
